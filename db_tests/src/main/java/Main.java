4223f594b24e7aff8c237097ab09e0d1bdc7e3b8
insomnia* <budljanskiy@mail.ru>
2018-11-02 03:20:02 +0500

    INIT COMMIT

---
+++ db_tests/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]){
+        var controller = new DatabaseController();
+//        controller.createNewTable();
+//        controller.addNewUser(1234);
+//        controller.getUserInfo(1234);
+//        controller.updateUserInfo(1234, IDatabaseController.UserInfo.last_class_num_request, 6);
+//        controller.removeUser(1234);
+    }
+}
